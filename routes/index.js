d51993e93c15bf7e92e05e1686861ea46bc4a47c
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-09-15 14:06:22 -0500

    added reddit like blocks of posts

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,10 @@ const router = Router();
 const Post = require("../models/post.js")
 
 router.get('/', (req, res) => {
-  res.render("index");
+  Post.find().sort({votes: 1})
+  .then((posts) => {
+    res.render("index", {posts} )
+  });
 });
 
 router.get("/new", (req, res) => {
