6b786b290b4889ef24c0af3b463c94adfda9ee26
Lauren Smith <laurensmith@comcast.net>
2016-09-16 11:52:53 -0500

    setting up deployment

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 
-const MONGODB_URL = "mongodb://localhost:27017/mlreddit";
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/mlreddit";
 
 mongoose.Promise = Promise;
 
